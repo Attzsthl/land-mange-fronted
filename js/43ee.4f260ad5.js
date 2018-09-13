5a159aea573d8094a7dddc9494b4b24997ea049f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-13 10:06:08 +0000

    Deploy d2-projects/d2-admin to github.com/d2-projects/d2-admin.git:gh-pages

--- js/43ee.4f260ad5.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["43ee"],{"43ee":function(e,t,n){"use strict";n.r(t);var l=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("d2-container",[n("h1",{staticClass:"d2-mt-0"},[e._v("该页面尚未完成组件示例搬运")]),n("p",[e._v("D2Admin 的构建依托于由饿了么出品的 ElementUI，欲了解更多该组件的信息请查阅以下链接")]),n("d2-link-btn",{attrs:{title:"Element",link:"http://element.eleme.io/#/zh-CN"}})],1)},i=[],s=n("2877"),a={},o=Object(s["a"])(a,l,i,!1,null,null,null);o.options.__file="index.vue";t["default"]=o.exports}}]);
-//# sourceMappingURL=43ee.4f260ad5.js.map
