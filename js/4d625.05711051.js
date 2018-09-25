87197d2601bc12fa0e9d4808784406c59236803e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-25 03:26:06 +0000

    Deploy d2-projects/d2-admin to github.com/d2-projects/d2-admin.git:gh-pages

--- js/4d625.05711051.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["4d625"],{"4d625":function(n,t,e){"use strict";e.r(t);var o=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("d2-container",[e("tree-view",{attrs:{data:n.packJson,options:n.options}})],1)},a=[],i=(e("cadf"),e("551c"),e("097d"),e("9224")),c={data:function(){return{options:{maxDepth:10,rootObjectKey:"package.json",modifiable:!1},packJson:i}}},s=c,p=e("2877"),r=Object(p["a"])(s,o,a,!1,null,null,null);r.options.__file="index.vue";t["default"]=r.exports}}]);
-//# sourceMappingURL=4d625.05711051.js.map
